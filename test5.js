b506a461448b44e7f2a2013d7680878046ca68a4
idmzulkarnaen <kreatifku1995@gmail.com>
2021-12-05 17:44:33 +0700

    test 5

--- test5.js
+++ test5.js
@@ -9,6 +9,22 @@ const words = ['flower', 'flow', 'flight'];
 
 function result(words) {
   // Your Code Here
+  let arrlength = [];
+
+  for (let i = 0; i < words.length; i++) {
+    arrlength.push(words[i].length);
+  }
+
+  let min = arrlength.reduce((a, b) => {
+    return Math.min(a, b);
+  });
+
+  words.sort();
+  for (let i = 0; i < min; i++) {
+    if (words[0][i] != words[words.length - 1][i]) {
+      return words[0].substring(0, i);
+    }
+  }
 }
 
 console.log(result(words));
