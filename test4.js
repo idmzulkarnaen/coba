b3cc64b83b70fa062240b95da72a49a17005e7b2
idmzulkarnaen <kreatifku1995@gmail.com>
2021-12-05 17:39:00 +0700

    test 4

--- test4.js
+++ test4.js
@@ -9,7 +9,14 @@ const numbers = [9, 6, 4, 2, 3, 5, 7, 0, 1];
 
 function result(numbers) {
   // Your Code Here
-  
+  let int = 0
+	for (let i = 0; i <= numbers.length; i++) {
+		if (numbers.includes(i) !== true) {
+			int = i
+			break
+		}
+	}
+	return int
 }
 
 console.log(result(numbers));
